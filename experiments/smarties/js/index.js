9489b8a9a52b9e650b421e76c930d803c6302dd0
Julien <Julienetie@gmail.com>
2015-07-15 22:23:12 +0100

    Initial commit

---
+++ experiments/smarties/js/index.js
@@ -0,0 +1,101 @@
+(function (root) {
+/*
+ * More fruity circle BS, enjoy ;-) 
+*/
+    // Options
+    opt = {
+        count: 32,
+        min: 0.3,
+        max: 0.8,
+        maxVel: 12, // Set this to 1
+        opacity: 0.5, // For mobile
+        filter: "difference" // multiply | exclusion
+    }
+
+    
+    
+    var canvas = document.getElementById('smarties'),
+        c = canvas.getContext('2d'),
+        space = [canvas.width, canvas.height],
+        cProps = [0, 0].concat(space),
+        rad = 2 * Math.PI,
+        rand,
+        collision = {};
+  
+/** Quick dirty trick to faill back on something less
+ *  cpu intensivie for mobile devices
+ */
+if(screen.width < 1023){
+   c.globalAlpha = opt.opacity;
+}else{
+  c.globalCompositeOperation = opt.filter;
+}
+    // Make circles
+    var circles = [];
+    for (var i = 0; i < opt.count; i++) {
+        rand = Math.random();
+        rand > opt.min ? rand : rand = opt.min;
+        var rDiff = (opt.max * 300) * rand;
+        circles.push({
+            x: ~~ ((space[0] * rand) - rDiff) || 25,
+            y: ~~ ((space[1] * rand) - rDiff) || 25,
+            r: ~~ (rDiff),
+            color: ~~ (360 * rand),
+            vx: ~~ (opt.maxVel * Math.random()),
+            vy: ~~ (opt.maxVel * rand)
+        });
+    }
+
+    
+    // Animation loop
+    var rAF = (function () {
+        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (loop) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+    })();
+
+// Switch
+var sw,count = 0;
+root.switchState = function(state){
+    if(!count){
+        sw = state;
+     rAF(paint);
+     count = 1;
+    }else{
+       sw = state
+       count = 0;
+    }
+}
+    // Paint circles
+    function paint() {
+        c.fillRect.apply(c, cProps);
+        c.clearRect.apply(c, cProps);
+        for (var i = 0; i < circles.length; i++) {
+            //var rand  = (circles[i].color /3.6) /2;
+            c.fillStyle = 'hsl(' + circles[i].color + ','+100+'%,'+50+'%)';
+            c.beginPath();
+            c.arc(circles[i].x, circles[i].y, circles[i].r, 0, rad, false);
+            c.fill();
+
+            // Collision logic
+            collision.right = circles[i].x + circles[i].r > space[0];
+            collision.left = circles[i].x - circles[i].r < 0;
+            collision.floor = circles[i].y + circles[i].r > space[1];
+            collision.ceiling = circles[i].y - circles[i].r < 0;
+
+            // Turn the other cheek 
+            collision.left || collision.right ? circles[i].vx *= -1 : null;
+            collision.floor || collision.ceiling ? circles[i].vy *= -1 : null;
+
+            circles[i].x += circles[i].vx; // horizontal force 
+            circles[i].y += circles[i].vy; // vertical force 
+        }
+        if(sw)
+            rAF(paint);
+    }
+
+  
+
+
+}(window));
+
---
+++ visuals/alt/alt.php
@@ -0,0 +1,75 @@
+<!doctype html>  
+<html lang=en>
+<head>
+<!-- 
++-+-+-+-+-+-+ +-+-+-+-+-+-+-+
+|J|u|l|i|e|n| |E|t|i|e|n|n|e|
++-+-+-+-+-+-+ +-+-+-+-+-+-+-+
+© Julien Etienne 2011
+
+Welcome Scavengers
+-->
+<meta charset="utf-8"> 
+<meta name="robots" content="noindex"/>
+<!--[if IE 8]> <meta http-equiv="X-UA-Compatible" content="IE=8" /> <![endif]-->
+<title>Alternative</title>
+<script type="text/javascript" src="../java/preloadalt.js"></script>
+<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js'></script>
+<script type="text/javascript" src="../java/jqFancyTran_cust.js"></script> 
+<link type="text/css" rel="stylesheet" href="../css/sister.css"/>
+<style>
+body {border:0px;outline:none;margin:0px;width:900px; height:300px;background:#000;overflow:hidden;}
+body div {outline:none;border:0px;}
+a:{outline:none;border:#000 solid 0px;}
+#ftHolder{display:none;}
+img{border:0px none;outline:none;}
+a:link{color:#000;text-decoration:none;outline:none;outline:none;}
+a:visited{color:#000;text-decoration:none;outline:none;}
+a:active{background-color:#000;text-decoration:none;outline:none;}
+a:hover{color:red;text-decoration:none;outline:none;}
+
+</style>
+</head> 
+<body> 
+ <div  id="preloader">
+<div  id="cent">
+<script type="text/javascript">
+<!-- begin hiding
+if (document.images) {
+	var preloadBar = ''
+	for (i = 0; i < yourImages.length-1; i++) {
+		preloadBar += '<img src="' + dots[0].src + '" width="' + coverage + '" height="' + heightOfPreloadBar + '" name="img' + i + '" align="absmiddle">'
+	}
+	preloadBar += '<img src="' + dots[0].src + '" width="' + (leftOverWidth+coverage) + '" height="' + heightOfPreloadBar + '" name="img' + (yourImages.length-1) + '" align="absmiddle">'
+	document.write(preloadBar)
+	loadImages()
+}
+// end hiding -->
+</script>
+</div>
+</div>
+
+
+
+
+<div id="ftHolder"> 
+ 
+	<div id="ft"> 
+        <img src="2.jpg" /> 
+		<img src="3.jpg" /> 
+		<img src="1.jpg" /> 	 
+		<img src="8.jpg" />
+		<img src="4.jpg" /> 
+		<img src="6.jpg" /> 
+		<img src="7.jpg" /> 
+		<img src="9.jpg" /> 
+		
+	</div> 
+	
+</div> 
+<script> 
+	$('#ft').jqFancyTransitions({ navigation: true, links : false, delay: 30000, width:900, height: 300 });
+ </script>
+<img onload="document.getElementById('ftHolder').style.display = 'block'" class="zero" src="9.jpg" /> 
+</body> 
+</html>
